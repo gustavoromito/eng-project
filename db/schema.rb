2cb7e1b19ec39a1a31613818c4a3f30dd7d9e1bf
Gustavo Romito <guromito@gmail.com>
2017-09-25 00:55:39 -0300

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,15 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20170925013401) do
 
-  create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", limit: 255
+    t.string "last_name", limit: 255
+    t.string "email", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "password"
+    t.string "provider", limit: 255
+    t.string "uid", limit: 255
+    t.string "password", limit: 255
   end
 
 end
