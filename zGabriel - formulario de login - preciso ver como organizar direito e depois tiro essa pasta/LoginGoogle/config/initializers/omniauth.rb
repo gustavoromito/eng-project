74004b6e91d83c6cb584ee28d8330de1b2c63594
MulanSales <luanmsacc@gmail.com>
2017-10-05 19:36:59 -0300

    Deletada pasta conflituosa #8

--- zGabriel - formulario de login - preciso ver como organizar direito e depois tiro essa pasta/LoginGoogle/config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '110684611936-m7lcbtku034ij5tsl2euunsnqb5rrp01.apps.googleusercontent.com', 'TYgiCvJhMBrpAhbJqD6SBOlU', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
