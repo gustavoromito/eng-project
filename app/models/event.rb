a335b13aa54ee15a814531c275f737ef1dc7e174
Gustavo Romito <guromito@gmail.com>
2017-10-08 22:50:45 -0300

    Created relationship between user and event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ApplicationRecord
+	has_many :interested_users, class_name: 'UserInterest', foreign_key: 'event_id'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006070527) do
+ActiveRecord::Schema.define(version: 20171009013743) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20171006070527) do
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_interests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_user_interests_on_event_id"
+    t.index ["user_id"], name: "index_user_interests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
