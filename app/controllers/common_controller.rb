0888c6b643027ab0df1425d93657ab5ad67abb3e
MulanSales <luanmsacc@gmail.com>
2017-10-19 23:20:54 -0200

    Código refatora, sem conflito entre ctrl #15

---
+++ app/controllers/common_controller.rb
@@ -0,0 +1,23 @@
+class CommonController < ApplicationController
+
+	def update(reference, message, referencep)
+	  respond_to do |format|
+      if reference.update(referencep)
+        format.html { redirect_to reference, notice: message }
+        format.json { render :show, status: :ok, location: reference }
+      else
+        format.html { render :edit }
+        format.json { render json: reference.errors, status: :unprocessable_entity }
+      end
+      end
+	end
+
+    def destroy(reference, message, redirectp)
+    	reference.destroy
+    	respond_to do |format|
+      		format.html { redirect_to redirectp, notice: message}
+      		format.json { head :no_content }
+    	end
+    end
+
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class PostsController < ApplicationController
+class PostsController < CommonController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
@@ -37,25 +37,13 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    super(@post, 'Post atualizado com sucesso',post_params);
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    super(@post, 'Post apagado com sucesso',posts_url)
   end
 
   private
