31d2fbf0eef9f4fc5b156866c091b4cec495ae1b
MulanSales <luanmsacc@gmail.com>
2017-10-25 20:08:03 -0200

    Adicionada imagens aos eventos #25

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
 	geocoded_by :address
 	after_validation :geocode
 	has_many :interested_users, class_name: 'UserInterest', foreign_key: 'event_id'
+	mount_uploader :image, AvatarUploader
 end
+
