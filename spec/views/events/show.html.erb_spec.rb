cb848004a4f3a6492355dd2f8d5b39dd4b9fa9e3
MulanSales <luanmsacc@gmail.com>
2017-11-02 15:00:11 -0200

    Testes de Views Event #35

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -3,22 +3,23 @@ require 'rails_helper'
 RSpec.describe "events/show", type: :view do
   before(:each) do
 
-    Category.new(
-      :name => "Type1",
-      :id => 1)
-
-    @categories = assign(:category, Category.all)
-
     @event = assign(:event, Event.create!(
       :title => "Title",
       :description => "MyText",
+      
     ))
+
+    @event.category = assign(:category, Category.new(
+        :name => "Type1",
+        :id => 1)
+    )
+
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/MyCategoryName/)
+    expect(rendered).to match(/Type1/)
   end
 end
