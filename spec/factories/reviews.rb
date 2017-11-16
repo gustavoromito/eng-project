5ff899a8912b21856419710195e7c45ad3f2c417
MulanSales <luanmsacc@gmail.com>
2017-11-16 19:48:35 -0200

    T2: Teste de Model Review #48

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :review do
-    
+    rate 5
+    commentary "Muito Bom!"
+    association :event, :user
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	
+	subject {
+		FactoryBot.create(:review)
+	}
+
+
+	it "é válido se tiver uma nota e comentário" do
+		expect(subject).to be_valid	
+	end
+
+	it "é válido se não tiver comentário" do
+
+	end
+
+	it "é inválido se não tiver uma nota" do
+
+	end
+
+	it "é válido se tiver um usuário e evento associados" do
+
+	end
+
+	it "é inválido se não tiver um usuário" do
+
+	end
+
+	it "é inválido se não tiver um evento" do
+
+	end
+
 end
