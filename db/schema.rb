c44a8e5761a00ccf55a83e1a582b9b40eacaebf7
Gustavo Romito <guromito@gmail.com>
2017-11-20 21:50:19 -0200

    Creating test for models

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 20171116223633) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
     t.string "avatar"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
@@ -81,11 +80,11 @@ ActiveRecord::Schema.define(version: 20171116223633) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", limit: 255
-    t.string "last_name", limit: 255
-    t.string "email_old", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email_old"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
     t.string "password"
--- spec/models/user_interest_spec.rb
+++ spec/models/user_interest_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe UserInterest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	describe '.create_interest' do
+
+		context 'when user is nil' do
+
+			let(:user) { create(:user) }
+
+			it 'does not create interest' do
+				expect {
+					UserInterest.create_interest(user, nil)
+				}.to change(UserInterest, :count).by(0)
+			end
+
+		end
+
+		context 'when event is nil' do
+
+			let(:event) { create(:event) }
+
+			it 'does not create interest' do
+				expect {
+					UserInterest.create_interest(nil, event)
+				}.to change(UserInterest, :count).by(0)
+			end
+
+		end
+
+	end
+
+	def self.create_interest(user, event)
+		interest = UserInterest.where(user: user, event: event)
+		return unless !interest || interest.count == 0
+
+		UserInterest.create(user: user, event: event)
+	end
+  
 end
