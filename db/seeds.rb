a0c26c4a369b753e937a1d4807da5628137fc701
MulanSales <luanmsacc@gmail.com>
2017-11-20 10:30:28 -0200

    db-heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Criando as Categorias
+category_sports = Category.create(name: "Esportes")
