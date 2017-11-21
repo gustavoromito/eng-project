ba224dfadf597e18d3f499c7e51f9bd90f8d2d86
Gustavo Romito <gustavo.romito@doghero.com.br>
2017-11-21 10:23:27 -0200

    Preventing SQL Injection

--- app/controllers/user_interest_controller.rb
+++ app/controllers/user_interest_controller.rb
@@ -1,8 +1,8 @@
 class UserInterestController < ApplicationController
 
 	def create
-	  u = User.find_by params[:user_id].to_i
-	  e = Event.find_by params[:event_id].to_i
+	  u = User.find_by id: params[:user_id].to_i
+	  e = Event.find_by id: params[:event_id].to_i
 
 	  @interest = UserInterest.new(user: u, event: e)
 	  respond_to do |format|
