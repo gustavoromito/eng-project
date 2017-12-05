847a0ee2ac9c71df07e5e109574cbbe25e4edee5
Gabriel <gabrielmichelini0206@hotmail.com>
2017-12-05 09:55:17 -0200

    Problemas corrigidos

--- features/checar_interesse.feature
+++ features/checar_interesse.feature
@@ -3,12 +3,12 @@
 
 Funcionalidade: Verificar se interesse em evento funciona
 	
-	Cenário: O usuário vai se interessar por um evento
-		Dado que sou um usuário válido
-		E que estou logado na minha conta
-		Quando eu vou para a página de eventos
+	Cenário: O usuário vai se logar no sistema criar um evento e se interessar por ele
+		Dado que sou usuário do sistema
+		E que estou logado no sistema
+		Quando vou para a página eventos
 		E apertar "New Event"
-		E preencher o campo endereço com endereco: "Parque Ibirapuera"
+		E preencher o campo endereço com: "Shopping Interlagos"
 		E apertar "Create Event"
 		Então o usuário é redirecionado para "eventos/1"
 		E clica em "ESTOU INTERESSADO!!!"
--- features/step_definitions/checar_interesse.rb
+++ features/step_definitions/checar_interesse.rb
@@ -1,9 +1,27 @@
 # encoding: utf-8
 
+Dado(/^que sou usuário do sistema$/) do
+  @user = User.create(:email => 'usuario@mail.com', :password => 'abc1234')
+end
+
+Dado(/^que estou logado no sistema$/) do
+  visit '/devise_scope/users/sign_in'
+  fill_in "Email", :with => @user.email
+  fill_in "Password", :with => @user.password
+  click_button "Log In"
+end
+
+Quando(/^vou para a página eventos$/) do
+  visit '/events'
+end
+
 E /^apertar "New Event"$/ do
   click_link('New Event')
 end
 
+E /^preencher o campo endereço com: "Shopping Interlagos"$/ do
+ 	fill_in "event[address]", :with => "Shopping Interlagos"
+end
 
 E /^apertar "Create Event"$/ do
   click_button('Create Event')
@@ -23,5 +41,5 @@ end
 
 Então(/^quando visito a página "Meus Eventos" o evento aparece lá$/) do
   Capybara.ignore_hidden_elements = false
-  expect(page).to have_content("Parque Ibirapuera")
+  expect(page).to have_content("Shopping Interlagos")
 end
